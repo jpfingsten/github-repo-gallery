5878214e1000be955d72d1852c2e2cbe8bbc6ac6
jpfingsten <jasmine.pfingsten@gmail.com>
2022-05-05 17:17:43 -0400

    Repos displaying, but need to debug names

--- js/script.js
+++ js/script.js
@@ -1,5 +1,6 @@
 const overview = document.querySelector(".overview") // overview div where profile info is displayed
-const username = "jpfingsten"; // my GitHub username
+const username = "jpfingsten"; // GitHub username
+const repoList = document.querySelector(".repo-list") // ul where repos are displayed
 
 // fetch user info from GitHub's REST API
 const fetchInfo = async function () {
@@ -11,18 +12,38 @@ const fetchInfo = async function () {
 
 fetchInfo();
 
-// display fetched info in overview div
-const fetchedInfo = function (userInfo) {
+// display fetched user info in overview div
+const fetchedInfo = function (info) {
     const div = document.createElement("div");
     div.classList.add("user-info");
     div.innerHTML = `<figure>
-        <img alt = "user avatar" src = ${userInfo.avatar_url} />
+        <img alt = "user avatar" src = ${info.avatar_url} />
     </figure >
     <div>
-        <p><strong>Name:</strong> ${userInfo.name}</p>
-        <p><strong>Bio:</strong> ${userInfo.bio}</p>
-        <p><strong>Location:</strong> ${userInfo.location}</p>
-        <p><strong>Number of public repos:</strong> ${userInfo.public_repos}</p>
+        <p><strong>Name:</strong> ${info.name}</p>
+        <p><strong>Bio:</strong> ${info.bio}</p>
+        <p><strong>Location:</strong> ${info.location}</p>
+        <p><strong>Number of public repos:</strong> ${info.public_repos}</p>
     </div>`;
     overview.append(div);
+
+    fetchRepos();
+}
+
+// fetch repos
+const fetchRepos = async function () {
+    const res = await fetch(`https://api.github.com/users/${username}/repos?sort=created&per_page=100`);
+    const repoData = await res.json();
+
+    displayRepos(repoData);
+}
+
+// loop through repos array and display info about repos
+const displayRepos = function (repos) {
+    for (const eachRepo of repos) {
+        const li = document.createElement("li");
+        li.classList.add("repo");
+        li.innerHTML = `<h3>${repos.name}</h3>`; //for some reason, this is returning "undefined"
+        repoList.append(li);
+    }
 }
